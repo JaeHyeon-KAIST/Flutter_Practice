71dc40955bca84a2772cd773a0e911cf6079035c
2 <alvinlog@kaist.ac.kr>
2021-12-21 05:06:01 +0900

    app connecting with map & location

---
+++ treasure_map/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MainMap(),
+    );
+  }
+}
+
+class MainMap extends StatefulWidget {
+  @override
+  _MainMapState createState() => _MainMapState();
+}
+
+class _MainMapState extends State<MainMap> {
+  final CameraPosition position = CameraPosition(
+    target: LatLng(37.2381676, 127.0708563),
+    zoom: 12,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Treasure Mapp'),
+      ),
+      body: Container(
+        child: GoogleMap(
+          initialCameraPosition: position,
+        ),
+      ),
+    );
+  }
+}
